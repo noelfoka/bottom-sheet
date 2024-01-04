cd5d9b531b569c03e60a05befce41040660f1d87
noelfoka <nomgnenoel@gmail.com>
2024-01-04 09:42:58 +0100

    set up project

---
+++ bottom-sheet.js
@@ -0,0 +1,20 @@
+import {LitElement, css, html} from 'lit';
+
+export class BottomSheet extends LitElement {
+  static properties = {
+    version: {},
+  };
+
+  constructor() {
+    super();
+    this.version = 'STARTING';
+  }
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    `;
+  }
+}
+customElements.define('bottom-sheet', BottomSheet);
